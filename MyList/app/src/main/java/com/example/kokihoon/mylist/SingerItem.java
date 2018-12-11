bd9ffc9846d6db36e18c2a35830e20e6fa1a9451
kokihoon <rhrlgns777@gmail.com>
2018-12-11 17:54:20 +0900

    Study Android ListView

---
+++ MyList/app/src/main/java/com/example/kokihoon/mylist/SingerItem.java
@@ -0,0 +1,48 @@
+package com.example.kokihoon.mylist;
+
+public class SingerItem {
+
+    private String name;
+    private String mobile;
+    int resId;
+
+    public int getResId() {
+        return resId;
+    }
+
+    public void setResId(int resId) {
+        this.resId = resId;
+    }
+
+    public SingerItem(String name, String mobile, int resId) {
+        this.name = name;
+        this.mobile = mobile;
+        this.resId =resId;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    @Override
+    public String toString() {
+        return "SingerItem{" +
+                "name='" + name + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", resId=" + resId +
+                '}';
+    }
+}
