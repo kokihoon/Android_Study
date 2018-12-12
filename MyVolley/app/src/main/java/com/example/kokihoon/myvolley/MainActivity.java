97b6649ccc1f91ace9c5daf1260e9243b9e676cb
kokihoon <rhrlgns777@gmail.com>
2018-12-13 03:52:02 +0900

    Study Android ImageLoad

--- MyVolley/app/src/main/java/com/example/kokihoon/myvolley/MainActivity.java
+++ MyVolley/app/src/main/java/com/example/kokihoon/myvolley/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.kokihoon.myvolley;
 
 import android.app.DownloadManager;
+import android.graphics.Bitmap;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -21,9 +24,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
-
     TextView textView;
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView)findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         Button button = (Button)findViewById(R.id.button);
 
@@ -41,14 +45,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        Button button2 = (Button)findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendImageRequset();
+            }
+        });
+
+
         if(AppHelper.requestQueue == null) {
             AppHelper.requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
     }
 
+    public void sendImageRequset() {
+
+        String url = "https://movie-phinf.pstatic.net/20180124_164/1516760031488tu7bS_JPEG/movie_image.jpg?type=m886_590_2";
+
+        ImageLoadTask task = new ImageLoadTask(url, imageView);
+        task.execute();
+    }
+
     public void sendRequest() {
-        //String url = "http://www.google.co.kr";
-        String url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101";
+        //String url = "https://www.google.co.kr";
+        String url = "https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101";
 
         StringRequest request = new StringRequest(
                 Request.Method.GET,
