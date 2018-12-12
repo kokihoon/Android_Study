a752a66f40f3e3fbfd95dc4af8e8b59d10aefaa4
kokihoon <rhrlgns777@gmail.com>
2018-12-13 03:08:29 +0900

    Study Android Volley

---
+++ MyVolley/app/src/main/java/com/example/kokihoon/myvolley/AppHelper.java
@@ -0,0 +1,12 @@
+package com.example.kokihoon.myvolley;
+
+import com.android.volley.RequestQueue;
+
+/**
+ *
+ */
+public class AppHelper {
+    public static RequestQueue requestQueue;
+
+
+}
---
+++ MyVolley/app/src/main/java/com/example/kokihoon/myvolley/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.kokihoon.myvolley;
+
+import android.app.DownloadManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.BufferedReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+
+        if(AppHelper.requestQueue == null) {
+            AppHelper.requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+    }
+
+    public void sendRequest() {
+        String url = "http://www.google.co.kr";
+
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        println("응답 -> " + response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        println("에러 -> " + error.getMessage());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+
+                return params;
+            }
+        };
+
+        request.setShouldCache(false);
+
+        AppHelper.requestQueue.add(request);
+
+        println("요청 보냄.");
+    }
+
+    public void println(String  data) {
+        textView.append(data + "\n");
+    }
+}
