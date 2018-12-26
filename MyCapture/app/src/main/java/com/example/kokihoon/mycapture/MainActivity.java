5a3c5650d76ff021366f00aaa46febe5a6edbcb8
kokihoon <rhrlgns777@gmail.com>
2018-12-27 04:04:01 +0900

    Study Android Capture

---
+++ MyCapture/app/src/main/java/com/example/kokihoon/mycapture/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.kokihoon.mycapture;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    CameraSurfaceView surfaceView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        surfaceView = (CameraSurfaceView) findViewById(R.id.surfaceView);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capture();
+            }
+        });
+    }
+
+    public void capture() {
+        surfaceView.capture(new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 8;
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+                imageView.setImageBitmap(bitmap);
+
+                camera.startPreview();
+            }
+        });
+    }
+}
