98ec2cda9078edd50b923fa66bf67870701be6a7
kokihoon <rhrlgns777@gmail.com>
2019-01-06 23:31:31 +0900

    Study Android Tab

---
+++ MyTab/app/src/main/java/com/example/kokihoon/mytab/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.kokihoon.mytab;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment fragment1;
+    Fragment fragment2;
+    Fragment fragment3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+
+        tabs.addTab(tabs.newTab().setText("친구"));
+        tabs.addTab(tabs.newTab().setText("일대일채팅"));
+        tabs.addTab(tabs.newTab().setText("기타"));
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+
+                Fragment selected = null;
+                if(position == 0) {
+                    selected = fragment1;
+                }else if(position == 1) {
+                    selected = fragment2;
+                }else if(position == 2) {
+                    selected = fragment3;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+}
